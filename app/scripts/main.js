861991e55c66d5c2f7aa5cdaf7098fe713b9771b
jgoley <jgoley@gmail.com>
2014-10-18 15:13:12 -0400

    Adds script which creates polyline on map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,28 @@
-var currentLocation = {};
+var currentLocation = {},
+    i = 0;
 
-function get_location() {
-    navigator.geolocation.getCurrentPosition(show_map);
+function get_location(i) {
+    navigator.geolocation.getCurrentPosition(getCoords);
 }
 
-function show_map(position) {
-    currentLocation.lat = position.coords.latitude;
-    currentLocation.lng = position.coords.longitude;
-    console.log(currentLocation.lat);
-    console.log(currentLocation.lng);
-    geoFire.set("test", [currentLocation.lat, currentLocation.lng]).then(function() {
-        console.log("Provided key has been added to GeoFire");
-    }, function(error) {
-        console.log("Error: " + error);
-    });
-
+function getCoords(position) {
+    i++
+    if (position) {
+        currentLocation.lat = position.coords.latitude;
+        currentLocation.lng = position.coords.longitude;
+        currentLocation.altitude = position.coords.altitude;
+        console.log(currentLocation.lng);
+        console.log(currentLocation.lat);
+        // setTimeout(function() {
+        //     geoFire.set("location" + i, [currentLocation.lat, currentLocation.lng]).then(function() {
+        //         console.log("Provided key has been added to GeoFire");
+        //     }, function(error) {
+        //         console.log("Error: " + error);
+        //     });
+        // }, 0);
+    } else {
+        console.log("not added");
+    }
 }
 
 var fireURL = "https://trailblazr.firebaseio.com/";
@@ -22,4 +30,12 @@ var fireURL = "https://trailblazr.firebaseio.com/";
 var fireRef = new Firebase(fireURL);
 var geoFire = new GeoFire(fireRef);
 
-get_location();
+//geoFire.child();
+
+setInterval(
+    function() {
+        get_location(i);
+    }, 5000
+);
+
+// var route = setInterval()
