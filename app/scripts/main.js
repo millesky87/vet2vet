2972dc9f480564e8dd2d28d05ae22b1717f9279f
jgoley <jgoley@gmail.com>
2014-10-18 11:55:07 -0400

    Implements posting data to firebase

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,25 @@
-function show_map(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  console.log(latitude);
-  console.log(longitude);
-}
+var currentLocation = {};
 
 function get_location() {
-  navigator.geolocation.getCurrentPosition(show_map);
+    navigator.geolocation.getCurrentPosition(show_map);
 }
+
+function show_map(position) {
+    currentLocation.lat = position.coords.latitude;
+    currentLocation.lng = position.coords.longitude;
+    console.log(currentLocation.lat);
+    console.log(currentLocation.lng);
+    geoFire.set("test", [currentLocation.lat, currentLocation.lng]).then(function() {
+        console.log("Provided key has been added to GeoFire");
+    }, function(error) {
+        console.log("Error: " + error);
+    });
+
+}
+
+var fireURL = "https://trailblazr.firebaseio.com/";
+
+var fireRef = new Firebase(fireURL);
+var geoFire = new GeoFire(fireRef);
+
+get_location();
